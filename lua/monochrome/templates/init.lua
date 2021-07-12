878ce8bb7a7946e1ebad4aa34e1df15d80ec139d
Dheepak Krishnamurthy <me@kdheepak.com>
2021-07-12 13:16:01 -0600

    Add iterm2 colorscheme generator

--- lua/monochrome/templates/init.lua
+++ lua/monochrome/templates/init.lua
@@ -27,43 +27,46 @@ end
 
 local CURRENT_FOLDER = debug.getinfo(1, 'S').source:sub(2):match('(.*[/\\])')
 
-function M.render(path, name)
+function M.generate_script(path)
+  local colors = require'monochrome'.colors
+  local iterm2_codes = {
+    fg = colors.fg,
+    bg = colors.bg,
+    selbg = colors.fg,
+    selfg = colors.bg,
+    curbg = colors.bg,
+    curfg = colors.neutral_blue,
+    black = colors.black,
+    red = colors.faded_red,
+    green = colors.faded_green,
+    yellow = colors.faded_yellow,
+    blue = colors.faded_blue,
+    magenta = colors.faded_purple,
+    cyan = colors.faded_aqua,
+    white = colors.white,
+    br_black = colors.gray1,
+    br_red = colors.bright_red,
+    br_green = colors.bright_green,
+    br_yellow = colors.bright_yellow,
+    br_blue = colors.bright_blue,
+    br_magenta = colors.bright_purple,
+    br_cyan = colors.bright_aqua,
+    br_white = colors.gray9,
+  }
+  local content = ''
+  for key, value in pairs(iterm2_codes) do
+    local s = [[\033]1337;SetColors=]] .. key .. '=' .. string.gsub(value, '#', '') .. [[\a]]
+    s = 'echo -ne "' .. s .. '"\n'
+    content = content .. s
+  end
+  writeFile(path, content)
+end
+
+function M.render(path)
   config.colors = require'monochrome'.colors
-  local colors = config.colors
-  local tpl = openTemplate(CURRENT_FOLDER .. name .. '.tpl')
+  local tpl = openTemplate(CURRENT_FOLDER .. 'iterm2.tpl')
   if tpl then
     print('=> Writing file ' .. path)
-    local iterm2_codes = {
-      fg = colors.fg,
-      bg = colors.bg,
-      selbg = colors.bg,
-      selfg = colors.neutral_blue,
-      curbg = colors.bg,
-      curfg = colors.neutral_blue,
-      black = colors.black,
-      red = colors.faded_red,
-      green = colors.faded_green,
-      yellow = colors.faded_yellow,
-      blue = colors.faded_blue,
-      magenta = colors.faded_purple,
-      cyan = colors.faded_aqua,
-      white = colors.white,
-      br_black = colors.gray1,
-      br_red = colors.bright_red,
-      br_green = colors.bright_green,
-      br_yellow = colors.bright_yellow,
-      br_blue = colors.bright_blue,
-      br_magenta = colors.bright_purple,
-      br_cyan = colors.bright_aqua,
-      br_white = colors.gray9,
-    }
-    local content = ''
-    for key, value in pairs(iterm2_codes) do
-      local s = [[\033]1337;SetColors=]] .. key .. '=' .. string.gsub(value, '#', '') .. [[\a]]
-      s = 'echo -ne "' .. s .. '"\n'
-      content = content .. s
-    end
-    writeFile('colors.sh', content)
     -- LuaFormatter off
     writeFile(path,
       tpl
